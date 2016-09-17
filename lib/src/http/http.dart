7077cf90891a0d34cbf6ca9919441263229fb43d
thosakwe <thosakwe@gmail.com>
2016-09-17 12:13:52 -0400

    Base

--- lib/src/http/http.dart
+++ lib/src/http/http.dart
@@ -3,6 +3,8 @@ library angel_framework.http;
 
 export 'angel_base.dart';
 export 'angel_http_exception.dart';
+export 'base_middleware.dart';
+export 'base_plugin.dart';
 export 'controller.dart';
 export 'hooked_service.dart';
 export 'metadata.dart';
