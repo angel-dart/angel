af8a02b07f99cf6b69006c6ccae050b90d7cd6b1
thosakwe <thosakwe@gmail.com>
2016-09-19 03:38:30 -0400

    Added diagnostics

--- bin/server.dart
+++ bin/server.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:angel/angel.dart';
+import 'package:angel_diagnostics/angel_diagnostics.dart';
 import 'package:angel_framework/angel_framework.dart';
+import 'package:intl/intl.dart';
 
 main() async {
   runZoned(startServer, onError: onError);
@@ -9,11 +11,12 @@ main() async {
 
 startServer() async {
   Angel app = await createServer();
+  DateFormat dateFormat = new DateFormat("y-MM-dd");
   InternetAddress host = new InternetAddress(app.properties['host']);
   int port = app.properties['port'];
+  var now = new DateTime.now();
 
-  await app.startServer(host, port);
-  print("Angel server listening on ${host.address}:${port}");
+  await new DiagnosticsServer(app, new File("logs/${dateFormat.format(now)}.txt")).startServer(host, port);
 }
 
 onError(error, [StackTrace stackTrace]) {
---
+++ logs/README.md
@@ -0,0 +1,2 @@
+# Logs
+This folder contains logs from each day. Once the server is started, it creates a log in this directory.
