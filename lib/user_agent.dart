2d2d08ecd7858b5c7c82504695bb6bc7751e85e2
Tobe O <thosakwe@gmail.com>
2016-12-11 23:27:25 -0500

    Update user_agent.dart

--- lib/user_agent.dart
+++ lib/user_agent.dart
@@ -3,4 +3,14 @@ library user_agent;
 import 'src/matchers.dart';
 import 'src/user_agent.dart';
 
+/// Parses the given header into a user agent.
 UserAgent parse(String header) {}
+
+class UserAgentException implements Exception {
+  final String message;
+  
+  UserAgentException(this.message);
+  
+  @override
+  String toString() => 'User Agent exception: $message';
+}
