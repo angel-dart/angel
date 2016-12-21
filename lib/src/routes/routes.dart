373a3a30c60b5440083c3c2860c3b75fa46606dc
thosakwe <thosakwe@gmail.com>
2016-12-21 14:06:09 -0500

    Alpha ready :)

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -2,6 +2,7 @@
 library angel.routes;
 
 import 'package:angel_cors/angel_cors.dart';
+import 'package:angel_errors/angel_errors.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_proxy/angel_proxy.dart';
 import 'package:angel_static/angel_static.dart';
@@ -19,14 +20,32 @@ configureRoutes(Angel app) async {
 }
 
 configureAfter(Angel app) async {
-  // 404 handler
-  app.after.add((req, ResponseContext res) async {
-    throw new AngelHttpException.NotFound();
+  // Set our application up to handle different errors.
+  var errors = new ErrorHandler(handlers: {
+    404: (req, res) async =>
+        res.render('error', {'message': 'No file exists at ${req.path}.'}),
+    500: (req, res) async => res.render('error', {'message': req.error.message})
   });
 
-  // Default error handler
-  app.onError(
-      (e, req, res) async => res.render('error', {'message': e.message}));
+  errors.fatalErrorHandler = (AngelFatalError e) async {
+    e.request.response
+      ..statusCode = 500
+      ..writeln('500 Internal Server Error: ${e.error}')
+      ..writeln(e.stack);
+    await e.request.response.close();
+  };
+
+  // Throw a 404 if no route matched the request
+  app.after.add(errors.throwError());
+
+  // Handle errors when they occur, based on outgoing status code.
+  // By default, requests will go through the 500 handler, unless
+  // they have an outgoing 200, or their status code has a handler
+  // registered.
+  app.after.add(errors.middleware());
+
+  // Pass AngelHttpExceptions through handler as well
+  await app.configure(errors);
 }
 
 configureServer(Angel app) async {
