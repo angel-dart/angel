55672d1fe9755863c7f9e7155889ea6a224a261d
Tobe O <thosakwe@gmail.com>
2016-12-25 19:33:16 -0500

    Update multi_server.dart

--- bin/multi_server.dart
+++ bin/multi_server.dart
@@ -3,7 +3,18 @@ import 'dart:io';
 import 'package:angel_multiserver/angel_multiserver.dart';
 
 final Uri cluster = Platform.script.resolve('cluster.dart');
-final errorPage = GZIP.encode(UTF8.encode('''
+final error502 = GZIP.encode(UTF8.encode('''
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>502 Bad Gateway</title>
+          </head>
+          <body>
+            <h1>502 Bad Gateway</h1>
+          </body>
+        </html>
+        '''));
+final error503 = GZIP.encode(UTF8.encode('''
         <!DOCTYPE html>
         <html>
           <head>
@@ -25,13 +36,25 @@ main() async {
       // Auto-spawn a new instance on crash
       loadBalancer.spawnIsolates(cluster);
     })
+    ..onErrored.listen((socket) async {
+       try {
+         socket
+           ..writeln('HTTP/1.1 502 Bad Gateway')
+           ..writeln(HttpDate.format(new DateTime.now()))
+           ..writeln('Content-Encoding: gzip')
+           ..writeln()
+           ..add(error502)
+           ..writeln();
+      await socket.close();
+       } catch(_) {}
+    })
     ..onUnavailable.listen((socket) async {
       socket
         ..writeln('HTTP/1.1 503 Service Unavailable')
         ..writeln(HttpDate.format(new DateTime.now()))
         ..writeln('Content-Encoding: gzip')
         ..writeln()
-        ..add(errorPage)
+        ..add(error503)
         ..writeln();
       await socket.close();
     });
