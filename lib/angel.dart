77c728dd7d00cbea01fb00e4b39eaa0d22936ca0
Tobe O <thosakwe@gmail.com>
2017-04-11 14:18:52 -0400

    Update angel.dart

--- lib/angel.dart
+++ lib/angel.dart
@@ -6,7 +6,6 @@ import 'package:angel_common/angel_common.dart';
 import 'src/config/config.dart' as configuration;
 import 'src/routes/routes.dart' as routes;
 import 'src/services/services.dart' as services;
-export 'src/services/services.dart';
 
 /// Creates and configures the server instance.
 Future<Angel> createServer() async {
