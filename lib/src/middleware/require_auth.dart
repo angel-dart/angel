2cae916e39dfa7227a3ba0794437a85c63318f64
Tobe O <thosakwe@gmail.com>
2017-10-04 18:01:21 -0400

    +1

--- lib/src/middleware/require_auth.dart
+++ lib/src/middleware/require_auth.dart
@@ -2,9 +2,6 @@ import 'dart:async';
 import 'dart:io';
 import 'package:angel_framework/angel_framework.dart';
 
-/// Restricts access to a resource via authentication. Constant instance.
-const RequestMiddleware requireAuth = const RequireAuthorizationMiddleware();
-
 /// Forces Basic authentication over the requested resource, with the given [realm] name, if no JWT is present.
 ///
 /// [realm] defaults to `'angel_auth'`.
@@ -22,23 +19,18 @@ RequestHandler forceBasicAuth({String realm}) {
 }
 
 /// Restricts access to a resource via authentication.
-class RequireAuthorizationMiddleware implements AngelMiddleware {
-  const RequireAuthorizationMiddleware();
-
-  @override
-  Future<bool> call(RequestContext req, ResponseContext res,
-      {bool throwError: true}) async {
-    bool _reject(ResponseContext res) {
-      if (throwError) {
-        res.statusCode = HttpStatus.FORBIDDEN;
-        throw new AngelHttpException.forbidden();
-      } else
-        return false;
-    }
-
-    if (req.properties.containsKey('user') || req.method == 'OPTIONS')
-      return true;
-    else
-      return _reject(res);
+Future<bool> requireAuth(RequestContext req, ResponseContext res,
+    {bool throwError: true}) async {
+  bool _reject(ResponseContext res) {
+    if (throwError) {
+      res.statusCode = HttpStatus.FORBIDDEN;
+      throw new AngelHttpException.forbidden();
+    } else
+      return false;
   }
+
+  if (req.properties.containsKey('user') || req.method == 'OPTIONS')
+    return true;
+  else
+    return _reject(res);
 }
