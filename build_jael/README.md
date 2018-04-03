f8fd0e0a0ad2f5526c8098137b3f943719bacdf8
Tobe O <thosakwe@gmail.com>
2018-04-03 11:29:35 -0400

    README, CHANGELOG

---
+++ build_jael/README.md
@@ -0,0 +1,44 @@
+# build_jael
+[![Pub](https://img.shields.io/pub/v/build_jael.svg)](https://pub.dartlang.org/packages/build_jael)
+[![build status](https://travis-ci.org/angel-dart/jael.svg)](https://travis-ci.org/angel-dart/jael)
+
+
+Compile Jael files to HTML using the power of `package:build`.
+
+# Installation
+In your `pubspec.yaml`:
+
+```yaml
+dependencies:
+  build_jael: ^1.0.0
+dev_dependencies:
+  build_runner: ^0.7.0
+```
+
+# Usage
+You can run `pub run build_runner serve` to incrementally build Jael templates,
+and run an HTTP server.
+
+For further customization, you'll need to either modify the `build.yaml` or
+instantiate a `JaelBuilder` manually.
+
+## Defining Variables
+Pass variables as `config` in `build.yaml`:
+
+```yaml
+targets:
+  $default:
+    builders:
+      build_jael:
+        config:
+          foo: bar
+          baz: quux
+          one: 1.0
+```
+
+## Minifying HTML
+Pass `minify: true` in the build configuration to produce "minified" HTML,
+without newlines or whitespace (other than where it is required).
+
+To apply additional transforms to parsed documents, provide a
+set of `patch` functions, like in `package:jael_preprocessor`.
--- build_jael/lib/build_jael.dart
+++ build_jael/lib/build_jael.dart
@@ -3,12 +3,16 @@ import 'package:build/build.dart';
 import 'package:code_buffer/code_buffer.dart';
 import 'package:file/file.dart';
 import 'package:jael/jael.dart' as jael;
-import 'package:jael_preprocessor/jael_preprocessor.dart';
+import 'package:jael_preprocessor/jael_preprocessor.dart' as jael;
+import 'package:symbol_table/symbol_table.dart';
+
+Builder jaelBuilder(BuilderOptions options) => new JaelBuilder(options);
 
 class JaelBuilder implements Builder {
   final BuilderOptions options;
+  final List<jael.Patcher> patch;
 
-  const JaelBuilder(this.options);
+  const JaelBuilder(this.options, {this.patch: const []});
 
   @override
   Map<String, List<String>> get buildExtensions {
@@ -36,6 +40,24 @@ class JaelBuilder implements Builder {
       onError: errors.add,
     );
 
-    if (errors.isNotEmpty) {}
+    doc = await jael.resolve(
+      doc,
+      dir,
+      onError: errors.add,
+      patch: this.patch,
+    );
+
+    if (errors.isNotEmpty) {
+      jael.Renderer.errorDocument(errors, buf);
+      return;
+    }
+
+    var scope = new SymbolTable(values: new Map.from(options.config));
+    const jael.Renderer().render(doc, buf, scope);
+
+    buildStep.writeAsString(
+      buildStep.inputId.changeExtension('.html'),
+      buf.toString(),
+    );
   }
 }
