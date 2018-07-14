df0632d0004953ddccfab0cf3ac96f4da30adda9
Tobe O <thosakwe@gmail.com>
2018-07-14 18:13:32 -0400

    Remove deprecated commands

--- lib/src/commands/commands.dart
+++ lib/src/commands/commands.dart
@@ -1,15 +1,10 @@
 library angel_cli.commands;
 
-export 'controller.dart';
 export "doctor.dart";
 export "key.dart";
 export "init.dart";
 export "install.dart";
 export "make.dart";
-export "plugin.dart";
 export "rename.dart";
-export "service.dart";
-export "start.dart";
-export "test.dart";
 export 'update.dart';
 export 'version.dart';
