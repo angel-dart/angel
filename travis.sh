f4a3bb22d3109f2b37c789e208e043331cda4ad2
Tobe O <thosakwe@gmail.com>
2018-08-03 17:25:48 -0400

    Update Travis to run ALL tests

--- travis.sh
+++ travis.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 cd graphql_parser && pub get && pub run test -j2 && cd ..
+cd graphql_schema && pub get && pub run test -j2 && cd ..
+cd graphql_server && pub get && pub run test -j2 && cd ..
