ecfd99e8651179f3197e031b8c0fd357d0d0a4e3
Tobe O <thosakwe@gmail.com>
2018-08-24 09:03:31 -0400

    Add basic annotation reader

---
+++ angel_orm_generator/lib/src/readers.dart
@@ -0,0 +1,19 @@
+import 'package:analyzer/dart/constant/value.dart';
+import 'package:angel_orm/angel_orm.dart';
+import 'package:source_gen/source_gen.dart';
+
+ORM reviveORMAnnotation(ConstantReader reader) {
+  return ORM(reader.peek('tableName')?.stringValue);
+}
+
+class ColumnReader {
+  final ConstantReader reader;
+
+  ColumnReader(this.reader);
+
+  bool get isNullable => reader.peek('isNullable')?.boolValue ?? true;
+
+  int get length => reader.peek('length')?.intValue;
+
+  DartObject get defaultValue => reader.peek('defaultValue')?.objectValue;
+}
