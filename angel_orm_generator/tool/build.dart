7ffb92ac30a8f037e336f02f3494ead0591b6b57
Tobe O <thosakwe@gmail.com>
2018-08-24 08:57:04 -0400

    Remove old build scripts

--- angel_orm_generator/tool/build.dart
+++
@@ -1,4 +0,0 @@
-import 'package:build_runner/build_runner.dart';
-import 'actions.dart';
-
-main() => build(actions, deleteFilesByDefault: true);
--- angel_orm_generator/tool/watch.dart
+++
@@ -1,4 +0,0 @@
-import 'package:build_runner/build_runner.dart';
-import 'actions.dart';
-
-main() => watch(actions, deleteFilesByDefault: true);
