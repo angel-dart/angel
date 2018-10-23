93d8a94fd4b0dd3daf59703551507628a291a2f0
Tobe O <thosakwe@gmail.com>
2018-10-23 12:01:05 -0400

    parser @ 1.1.1

--- graphql_parser/CHANGELOG.md
+++ graphql_parser/CHANGELOG.md
@@ -1,3 +1,6 @@
+# 1.1.1
+* Pubspec updates for Dart 2.
+
 # 1.1.0
 * Removed `GraphQLVisitor`.
 * Enable parsing operations without an explicit
--- graphql_parser/test/common.dart
+++ graphql_parser/test/common.dart
@@ -1,5 +1,3 @@
 import 'package:graphql_parser/graphql_parser.dart';
-import 'package:matcher/matcher.dart';
-import 'package:test/test.dart';
 
 Parser parse(String text) => new Parser(scan(text));
