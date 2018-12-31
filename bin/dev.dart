09861238a18eac21c63910d0df4a094bac04fa3f
Tobe Osakwe <thosakwe@gmail.com>
2018-12-30 20:40:24 -0500

    Update dev.dart

--- bin/dev.dart
+++ bin/dev.dart
@@ -1,21 +1,24 @@
 import 'dart:io';
+import 'package:angel/src/pretty_logging.dart';
 import 'package:angel/angel.dart';
 import 'package:angel_container/mirrors.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_hot/angel_hot.dart';
-import 'package:lumberjack/lumberjack.dart';
-import 'package:lumberjack/io.dart';
+import 'package:logging/logging.dart';
 
 main() async {
   // Watch the config/ and web/ directories for changes, and hot-reload the server.
-  var hot = HotReloader(() async {
-    var app = Angel(reflector: MirrorsReflector());
+  var hot = new HotReloader(() async {
+    var app = new Angel(reflector: MirrorsReflector());
     await app.configure(configureServer);
-    app.logger = Logger('angel')..pipe(AnsiLogPrinter.toStdout());
-    app.shutdownHooks.add((_) => app.logger.close());
+    hierarchicalLoggingEnabled = true;
+    app.logger = new Logger('angel');
+    var sub = app.logger.onRecord.listen(prettyLog);
+    app.shutdownHooks.add((_) => sub.cancel());
+    return app;
   }, [
-    Directory('config'),
-    Directory('lib'),
+    new Directory('config'),
+    new Directory('lib'),
   ]);
 
   var server = await hot.startServer('127.0.0.1', 3000);
