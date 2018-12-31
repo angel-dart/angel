1f4c46d18a1afa2af4a492b82973335d8fe756a6
Tobe Osakwe <thosakwe@gmail.com>
2018-12-31 11:15:21 -0500

    Update prod.dart

--- bin/prod.dart
+++ bin/prod.dart
@@ -1,4 +1,4 @@
 import 'package:angel/angel.dart';
 import 'package:angel_production/angel_production.dart';
 
-main(List<String> args) => Runner('angel', configureServer).run(args);
+main(List<String> args) => Runner('{{angel}}', configureServer).run(args);
