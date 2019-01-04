3bae345297f0d56c21093838303d9aa673e4f437
Tobe Osakwe <thosakwe@gmail.com>
2019-01-04 09:41:41 -0500

    Update dev.dart

--- bin/dev.dart
+++ bin/dev.dart
@@ -1,14 +1,16 @@
 import 'dart:io';
 import 'package:angel/src/pretty_logging.dart';
 import 'package:angel/angel.dart';
+import 'package:angel_container/mirrors.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_hot/angel_hot.dart';
+import 'package:io/ansi.dart';
 import 'package:logging/logging.dart';
 
 main() async {
-  // Watch the config/ and web/ directories for changes, and hot-reload the server.
-  var hot = new HotReloader(() async {
-    var app = new Angel();
+    // Watch the config/ and web/ directories for changes, and hot-reload the server.
+    var hot = new HotReloader(() async {
+    var app = new Angel(reflector: MirrorsReflector());
     await app.configure(configureServer);
     hierarchicalLoggingEnabled = true;
     app.logger = new Logger('angel');
@@ -21,5 +23,9 @@ main() async {
   ]);
 
   var server = await hot.startServer('127.0.0.1', 3000);
+  
+  print(red.wrap('NOTICE: You initialized this project with an outdated version of the Angel CLI.'));
+  print(red.wrap('It is recommended that you run `pub global activate angel_cli` before creating future projects.'));
+  
   print('Listening at http://${server.address.address}:${server.port}');
 }
