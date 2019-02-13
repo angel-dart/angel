a7b6b17114a102ca64a3ae67ffd41bb1d5d00c23
Tobe O <thosakwe@gmail.com>
2019-02-13 08:07:16 -0500

    start orm_test

---
+++ angel_orm_test/lib/src/models/todo.dart
@@ -0,0 +1,12 @@
+import 'package:angel_migration/angel_migration.dart';
+import 'package:angel_serialize/angel_serialize.dart';
+import 'package:angel_orm/angel_orm.dart';
+part 'todo.g.dart';
+
+@serializable
+@orm
+abstract class _Todo extends Model {
+  String get text;
+
+  bool get isComplete;
+}
