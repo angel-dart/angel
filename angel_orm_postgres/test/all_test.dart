dbb2487c5cc0433fd49e51fd99d78659d88188a0
Tobe O <thosakwe@gmail.com>
2019-02-14 14:23:37 -0500

    add insertion test

--- angel_orm_postgres/test/all_test.dart
+++ angel_orm_postgres/test/all_test.dart
@@ -1,6 +1,27 @@
+import 'dart:async';
+
 import 'package:angel_orm_postgres/angel_orm_postgres.dart';
+import 'package:angel_orm_test/angel_orm_test.dart';
+import 'package:postgres/postgres.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
+  PostgreSQLConnection _connect() {
+    return PostgreSQLConnection('localhost', 5432, 'angel_orm_test');
+  }
+
+  group('single', () {
+    PostgreSqlExecutor executor;
+    PostgreSQLConnection c = _connect();
+
+    setUp(() async {
+      var c = _connect();
+      await c.open();
+      executor = PostgreSqlExecutor(c);
+    });
+
+    tearDown(() => executor.close());
+
+    ormTests(() => executor);
+  });
 }
--- angel_orm_test/lib/src/cases/cases.dart
+++ angel_orm_test/lib/src/cases/cases.dart
@@ -2,8 +2,6 @@ import 'package:angel_orm/angel_orm.dart';
 import 'package:test/test.dart';
 import 'standalone.dart';
 
-void Function() ormTests(QueryExecutor executor) {
-  return () {
-    group('standalone', standaloneTests(executor));
-  };
+void ormTests(QueryExecutor Function() executor) {
+  group('standalone', standaloneTests(executor));
 }
