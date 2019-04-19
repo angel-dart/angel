34e5578676316a4b6ee117b6e01ca13bb70c3c5a
Tobe O <thosakwe@gmail.com>
2019-04-18 23:20:41 -0400

    Travis

--- travis.sh
+++ travis.sh
@@ -3,3 +3,4 @@ PWD=$(pwd)
 cd "$PWD/graphql_parser" && pub get && pub run test -j2 && cd ..
 cd "$PWD/graphql_schema" && pub get && pub run test -j2 && cd ..
 cd "$PWD/graphql_server" && pub get && pub run test -j2 && cd ..
+cd "$PWD/data_loader" && pub get && pub run test -j2 && cd ..
