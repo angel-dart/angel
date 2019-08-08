b1787d9c8de9ccf736ad5e6186f605ac5bf0496b
Tobe O <thosakwe@gmail.com>
2019-08-07 22:31:23 -0400

    pedantic: ast/default_value.dart

--- graphql_parser/lib/src/language/ast/default_value.dart
+++ graphql_parser/lib/src/language/ast/default_value.dart
@@ -3,12 +3,20 @@ import '../token.dart';
 import 'input_value.dart';
 import 'node.dart';
 
+/// The default value to be passed to an [ArgumentContext].
 class DefaultValueContext extends Node {
-  final Token EQUALS;
+  /// The source token.
+  final Token equals;
+
+  /// The default value for the argument.
   final InputValueContext value;
 
-  DefaultValueContext(this.EQUALS, this.value);
+  DefaultValueContext(this.equals, this.value);
+
+  /// Use [equals] instead.
+  @deprecated
+  Token get EQUALS => equals;
 
   @override
-  FileSpan get span => EQUALS.span.expand(value.span);
+  FileSpan get span => equals.span.expand(value.span);
 }
