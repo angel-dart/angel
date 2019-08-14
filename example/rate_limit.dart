c7434c220c0c732f4d2436a2c7717d3018bf8eec
Tobe O <thosakwe@gmail.com>
2019-08-14 14:49:14 -0400

    Remove idea

---
+++ example/rate_limit.dart
@@ -0,0 +1,7 @@
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_framework/http.dart';
+import 'package:angel_security/angel_security.dart';
+
+main() async {
+  var app = Angel(), http = AngelHttp(app);
+}
--- lib/src/rate_limiter.dart
+++ lib/src/rate_limiter.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
 import 'package:angel_framework/angel_framework.dart';
 
 /// A base class that facilitates rate limiting API's or endpoints,
 /// typically to prevent spam and abuse.
-abstract class RateLimiter {
+///
+/// The rate limiter operates under the assumption that a [User] object
+/// can be computed from each request, as well as information about
+/// the current rate-limiting window.
+abstract class RateLimiter<User> {
   /// The maximum number of requests allowed within the given [window].
   final int maxRequestsPerWindow;
 
