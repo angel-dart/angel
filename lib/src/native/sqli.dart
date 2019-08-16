7d3e4dc28b10703b898d2b2eb3182ab19473ac06
Tobe O <thosakwe@gmail.com>
2019-08-16 10:49:18 -0400

    SQL filter middleware

--- lib/src/native/sqli.dart
+++ lib/src/native/sqli.dart
@@ -9,6 +9,28 @@ LibInjectionScore sqlInjectionScore(String text) {
   return LibInjectionScore(result[0] as bool, result[1] as String);
 }
 
+/// Uses `libinjection` to filter out possible SQL injections from the
+/// query parameters ([RequestContext.queryParameters]).
+/// 
+/// Note: This is *destructive*, and modifies the query parameter map,
+/// instead of returning new data.
+bool sqliFilterQuery(RequestContext req, ResponseContext res) {
+  var out = <String, dynamic>{};
+  req.queryParameters.forEach((k, v) {
+    if (v is! String) {
+      out[k] = v;
+    } else {
+      var score = sqlInjectionScore(v as String);
+      if (!score.isInjection) {
+        out[k] = v;
+      }
+    }
+  });
+
+  req.queryParameters..clear()..addAll(out);
+  return true;
+}
+
 class LibInjectionScore {
   final bool isInjection;
   final String signature;
