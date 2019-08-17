9a97b93eced7b0fdf37459f6ed9ccc5f5fbdcea8
Tobe O <thosakwe@gmail.com>
2019-08-17 18:46:22 -0400

    Move MapQueryValues to the proper file

--- angel_orm/lib/src/query_values.dart
+++ angel_orm/lib/src/query_values.dart
@@ -57,11 +57,3 @@ abstract class QueryValues {
     return b.toString();
   }
 }
-
-/// A [QueryValues] implementation that simply writes to a [Map].
-class MapQueryValues extends QueryValues {
-  final Map<String, dynamic> values = {};
-
-  @override
-  Map<String, dynamic> toMap() => values;
-}
