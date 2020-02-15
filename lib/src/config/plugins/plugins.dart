6f6510ab27cf4de2222ad8699919e2d7ad42ee48
Tobe O <thosakwe@gmail.com>
2020-02-15 17:59:23 -0500

    clear out boilerplate files/folders

--- lib/src/config/plugins/plugins.dart
+++
@@ -1,9 +0,0 @@
-/// Custom plugins go here.
-library angel.src.config.plugins;
-
-import 'dart:async';
-import 'package:angel_framework/angel_framework.dart';
-
-Future configureServer(Angel app) async {
-  // Include any plugins you have made here.
-}
--- lib/src/routes/controllers/controllers.dart
+++
@@ -1,8 +0,0 @@
-library angel.src.routes.controllers;
-
-import 'dart:async';
-import 'package:angel_framework/angel_framework.dart';
-
-Future configureServer(Angel app) async {
-  /// Controllers will not function unless wired to the application!
-}
